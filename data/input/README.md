8e75d00c624a79601cf030f24c2b1b9022361d41
Alex-Karmazin <95565703+Alex-Karmazin@users.noreply.github.com>
2022-09-04 16:57:33 +0300

    Delete data directory
    Deleted unused folder

--- data/input/README.md
+++
@@ -1,4 +0,0 @@
-**<code>longevity_genes_original.csv</code>** - original data from [https://genomics.senescence.info](https://genomics.senescence.info/longevity/)
-**<code>longevity_genes_fix.csv</code>** - manually data from [https://genomics.senescence.info](https://genomics.senescence.info/longevity/)
-
-
--- data/input/post_fixes.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO snps(id, rsid, chrom, pos, ref, alt) VALUES (4452, 'Mt3010G/A', 'chrM', 3010, 'G', 'A');
-INSERT INTO snps(id, rsid, chrom, pos, ref, alt) VALUES (4453, 'Mt5178A/C', 'chrM', 5178, 'A', 'C');
-INSERT INTO snps(id, rsid, chrom, pos, ref, alt) VALUES (4454, 'Mt8414C/T', 'chrM', 8414, 'C', 'T');
