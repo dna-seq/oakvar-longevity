d40147e53d6f8e9f5c3e775eff4df71485a829c2
Alex-Karmazin <karmazinalex@gmail.com>
2022-10-18 19:59:46 +0300

    added data filess

---
+++ utility_scripts/utility_data/post_fixes.sql
@@ -0,0 +1,3 @@
+INSERT INTO snps(id, rsid, chrom, pos, ref, alt) VALUES (4452, 'Mt3010G/A', 'chrM', 3010, 'G', 'A');
+INSERT INTO snps(id, rsid, chrom, pos, ref, alt) VALUES (4453, 'Mt5178A/C', 'chrM', 5178, 'A', 'C');
+INSERT INTO snps(id, rsid, chrom, pos, ref, alt) VALUES (4454, 'Mt8414C/T', 'chrM', 8414, 'C', 'T');
